c68a37abc90dd530334c6737be2f292d189e2aab
ChanSon0211 <111247124+ChanSon0211@users.noreply.github.com>
2023-06-15 16:30:39 +0700

    add store and hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Users/Login';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+   <BrowserRouter>
+   <Routes>
+<Route index path='' element={<Login/>}/>
+
+
+
+
+   </Routes>
+   
+   
+   </BrowserRouter>
   );
 }
 
