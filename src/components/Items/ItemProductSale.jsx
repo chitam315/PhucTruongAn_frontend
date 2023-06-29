4586fad322d7c1146896c750b80679f70bc29f4a
chitam315 <ITITIU19048@student.hcmiu.edu.vn>
2023-06-29 10:23:50 +0700

    fix all conflict

--- src/components/Items/ItemProductSale.jsx
+++ src/components/Items/ItemProductSale.jsx
@@ -23,54 +23,10 @@ export default function ItemProductSale({ item }) {
             className="img-product h-full max-w-100"
           />
         </Link>
-<<<<<<< HEAD
         <div className="tag-status-sold uppercase flex justify-center text-white select-none">
           <span className="z-[50] text-[0.8em]">Đã bán: 3996</span>
         </div>
         <ProductInfo item={item} />
-=======
-
-        <Link to="/detail">
-          <div className="tag-status-sold uppercase flex justify-center text-white select-none">
-            <span className="z-[100] text-[0.8em]">Đã bán: 3996</span>
-            <div className="col-5w col-tb-4 col-tbs-6 col-sm-6 p-minus-10">
-              <div className="p-[10px]">
-                <div className="rounded-10 shadow-md bg-white rounded-[10px] h-100 relative p-[8px] block card-product">
-                  <div className="cursor-pointer cart-appear z-[10] text-[1.8em] text-white absolute right-[-15%] top-[47%] rounded-[50%] bg-[var(--mainColor)] p-[8px]">
-                    <AiOutlineShoppingCart />
-                  </div>
-                  <p className="text-black text-[16px] font-bold  h-[56px] name-product">
-                    [600W] Đèn UFO Năng Lượng Mặt Trời TP Solar TP-U600RGB Chiếu
-                    Sáng 360 Độ
-                  </p>
-                  <div className="text-[var(--accentColor2)] mb-[0.5em]">
-                    <span className="font-bold">1.950.000₫</span>
-                    <del className="text-[1.1em] text-[var(--accentColor1)] ml-[5px]">
-                      2.150.000₫
-                    </del>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Link>
-
-        <label className="flex items-center checkbox-compare relative cursor-pointer w-fit">
-          <input type="checkbox" className="cursor-pointer" />
-          <AiFillCheckCircle className="absolute top-[16%] w-[17px] h-[17px] text-[#3246ff] hidden" />
-          <p className="p-0">So sánh</p>
-        </label>
-        <label className="flex items-center checkbox-compare relative cursor-pointer w-fit">
-          <input
-            type="checkbox"
-            name="check"
-            id=""
-            className="cursor-pointer"
-          />
-          <AiFillCheckCircle className="absolute w-[17px] h-[17px] text-[#3246ff] hidden" />
-          <p>So sánh</p>
-        </label>
->>>>>>> 3dd8a64268824c9ae2c6c66258d35c50eea773d6
       </div>
     </div>
   );
--- src/components/Modal/ModalActiveGuarantee.jsx
+++ src/components/Modal/ModalActiveGuarantee.jsx
@@ -76,105 +76,6 @@ const ModalActiveGuarantee = ({ name, content }) => {
               </tr>
             </tbody>
           </table> */}
-<<<<<<< HEAD
-
-          <div className="row w-full justify-between items-center">
-            {/* <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center"> */}
-            <div className="row">
-              <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px] justify-start flex items-center">
-                Mã bảo hành *
-              </label>
-              <input
-                type="text"
-                placeholder="Nhập mã bảo hành"
-                className="center w-[calc(100%-190px)] border rounded-[5px] m-[10px] text-center text-[16px] p-[5px]"
-                className="center w-[160px]"
-              />
-            </div>
-            <div className="row w-full justify-between items-center">
-              <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                Tên khách hàng *
-              </label>
-              <input
-                required
-                type="text"
-                placeholder="Tên khách hàng"
-                className="center w-[calc(100%-190px)] border rounded-[5px] m-[10px] text-center text-[16px] p-[5px]"
-              />
-            </div>
-            <div className="row w-full justify-between items-center">
-              <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                Số điện thoại *
-              </label>
-              <input
-                required
-                type="number"
-                placeholder="Số điện thoại"
-                className="center w-[calc(100%-190px)] border rounded-[5px] m-[10px] text-center text-[16px] p-[5px]"
-              />
-            </div>
-            <div className="row w-full flex justify-between items-center">
-              <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                Thành phố
-              </label>
-              <select
-                className="form-select form-select-sm mb-3 w-[calc(100%-190px)] m-[10px]"
-                id="city"
-                aria-label=".form-select-sm"
-              >
-                <option value="" selected>
-                  Chọn tỉnh thành
-                </option>{" "}
-              </select>
-              =======
-              <div className="row">
-                <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px] justify-start flex items-center">
-                  Mã bảo hành *
-                </label>
-                <input
-                  type="text"
-                  placeholder="Nhập mã bảo hành"
-                  className="center w-[160px]"
-                />
-              </div>
-              <div className="row w-full justify-between items-center">
-                <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                  Tên khách hàng *
-                </label>
-                <input
-                  required
-                  type="text"
-                  placeholder="Tên khách hàng"
-                  className="center w-[calc(100%-190px)] border rounded-[5px] m-[10px] text-center text-[16px] p-[5px]"
-                />
-              </div>
-              <div className="row w-full justify-between items-center">
-                <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                  Số điện thoại *
-                </label>
-                <input
-                  required
-                  type="number"
-                  placeholder="Số điện thoại"
-                  className="center w-[calc(100%-190px)] border rounded-[5px] m-[10px] text-center text-[16px] p-[5px]"
-                />
-              </div>
-              <div className="row w-full flex justify-between items-center">
-                <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                  Thành phố
-                </label>
-                <select
-                  className="form-select form-select-sm mb-3 w-[calc(100%-190px)] m-[10px]"
-                  id="city"
-                  aria-label=".form-select-sm"
-                >
-                  <option value="" selected>
-                    Chọn tỉnh thành
-                  </option>{" "}
-                </select>
-
-                {/* <select className="w-[160px]">
-=======
           <div className="row">
             <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px] justify-start flex items-center">
               Mã bảo hành *
@@ -222,7 +123,6 @@ const ModalActiveGuarantee = ({ name, content }) => {
             </select>
 
             {/* <select className="w-[160px]">
->>>>>>> 3dd8a64268824c9ae2c6c66258d35c50eea773d6
               <option value="" selected>
                 Chọn thành phố
               </option>
@@ -232,39 +132,6 @@ const ModalActiveGuarantee = ({ name, content }) => {
               <option value="Camau">Cà Mau</option>
               <option value="Cantho">Cần Thơ</option>
             </select> */}
-<<<<<<< HEAD
-              </div>
-              <div className="row w-full flex justify-between items-center">
-                <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                  Thành phố
-                </label>
-                <select
-                  className="form-select form-select-sm mb-3  w-[calc(100%-190px)] m-[10px]"
-                  id="district"
-                  aria-label=".form-select-sm"
-                >
-                  <option value="" selected>
-                    Chọn quận huyện
-                  </option>
-                </select>
-              </div>
-              <div className="row w-full justify-between items-center">
-                <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
-                  Địa chỉ
-                </label>
-                <input
-                  required
-                  type="text"
-                  placeholder="Địa chỉ"
-                  className="center w-[calc(100%-190px)] border rounded-[5px] m-[10px] text-center text-[16px] p-[5px]"
-                />
-              </div>
-              <button className="uppercase m-[20px] px-[15px] h-[40px] text-[16px] rounded-[2px] bg-[#101C2C] border border-solid border-black text-white cursor-pointer block hover:bg-[#ff0000]">
-                kích hoạt
-              </button>
-            </div>
-=======
->>>>>>> 3dd8a64268824c9ae2c6c66258d35c50eea773d6
           </div>
           <div class="row w-full flex justify-between items-center">
             <label className="font-bold text-[14px] text-[#000] w-[160px] h-[40px]  flex items-center">
