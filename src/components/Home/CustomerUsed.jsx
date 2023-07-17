656b4d434c3987b69a258447f33d1af5b49832f5
chitam315 <ITITIU19048@student.hcmiu.edu.vn>
2023-07-17 18:03:06 +0700

    fix server

--- src/components/Home/CustomerUsed.jsx
+++ src/components/Home/CustomerUsed.jsx
@@ -3,6 +3,34 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 export default function CustomerUsed() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+    },
+  };
+  
+  const imgCustomer = [
+    {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_11.png?1685679333702"},
+    {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_12.png?1685679333702"},
+    {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_13.png?1685679333702"},
+    {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_14.png?1685679333702"},
+    {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_15.png?1685679333702"},
+  
+  ]
   return (
     <CardHome title="+78.000 KHÁCH HÀNG ĐÃ SỬ DỤNG">
       <span className="w-100 h-[1px] bg-[#ccc] block"></span>
@@ -24,31 +52,4 @@ export default function CustomerUsed() {
   );
 }
 
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 3,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 2,
-  },
-};
 
-const imgCustomer = [
-  {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_11.png?1685679333702"},
-  {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_12.png?1685679333702"},
-  {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_13.png?1685679333702"},
-  {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_14.png?1685679333702"},
-  {url:"https://bizweb.dktcdn.net/100/463/111/themes/889675/assets/gallery_bn_15.png?1685679333702"},
-
-]
